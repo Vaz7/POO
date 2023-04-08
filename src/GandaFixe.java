3dfe8103d19b7977291566e50b651a2cc628b864
Goncalo <a90969@alunos.uminho.pt>
2023-04-08 21:26:05 +0100

    Implementação da Classe Sapatilha, lixo na main para fazer testes.

--- src/GandaFixe.java
+++ src/GandaFixe.java
@@ -1,5 +1,15 @@
+import java.time.LocalDate;
+
 public class GandaFixe {
     public static void main(String[] args) {
-        System.out.println("ganda fixe");
+        LocalDate data = LocalDate.now().minusYears(3);
+        Sapatilha test2 = new Sapatilha(42, true,"Branco", data.plusYears(1), true, false, Sapatilha.Estado.BOM, 1, 450);
+        Sapatilha test = new Sapatilha(46, true,"Branco", data, false, false, Sapatilha.Estado.BOM, 3, 112);
+        Sapatilha test3 = new Sapatilha(42, true,"Branco", data.plusYears(1), true, false, Sapatilha.Estado.BOM, 1, 450);
+        System.out.println(test2.toString());
+        System.out.println(test.toString());
+
+        System.out.println(test2.equals(test3));
+        System.out.println(test.equals(test2));
     }
 }
