152da4d2df67568b9f877167957e80dd2465d5bf
Goncalo <a90969@alunos.uminho.pt>
2023-04-10 01:09:10 +0100

    Criação classe utilizador (ainda não testado, im done for today) com métodos para adição/remoção nos sets. Optei por HashSets por não ser necessário ter ordem e por ser mais fácil encontrar os artigos que queremos em vez de ter de percorrer a lista toda.

---
+++ src/Utilizador.java
@@ -0,0 +1,214 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Utilizador {
+    private int code;
+    private String email;
+    private String nome;
+    private String morada;
+    private int nif;
+    private Set<Artigo> vendido;
+    private Set<Artigo> para_vender;
+
+    private double dinheiro_vendas;
+    private double dinheiro_compras;
+
+    public Utilizador(){
+        this.code = 0;
+        this.email = "";
+        this.morada = "";
+        this.nif = 0;
+        this.vendido = new HashSet<>();
+        this.para_vender = new HashSet<>();
+        this.dinheiro_compras = 0;
+        this.dinheiro_vendas = 0;
+    }
+
+    public Utilizador(int code, String email, String nome, String morada, int nif, Set<Artigo> vendido, Set<Artigo> para_vender, double dinheiro_vendas, double dinheiro_compras) {
+        this.code = code;
+        this.email = email;
+        this.nome = nome;
+        this.morada = morada;
+        this.nif = nif;
+        setVendido(vendido);
+        setPara_vender(para_vender);
+        this.dinheiro_vendas = dinheiro_vendas;
+        this.dinheiro_compras = dinheiro_compras;
+    }
+
+    public Utilizador(Utilizador o){
+        this.code = o.getCode();
+        this.email = o.getEmail();
+        this.nome = o.getNome();
+        this.morada = o.getMorada();
+        this.nif = o.getNif();
+        this.vendido = o.getVendido();
+        this.para_vender = o.getPara_vender();
+        this.dinheiro_compras = o.getDinheiro_compras();
+        this.dinheiro_vendas = o.getDinheiro_vendas();
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public int getNif() {
+        return this.nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+
+    public Set<Artigo> getVendido() {
+        Set<Artigo> novo = new HashSet<>();
+        for(Artigo c : this.vendido){
+            novo.add(c);
+        }
+        return novo;
+    }
+
+    public void setVendido(Set<Artigo> vendido) {
+        this.vendido = new HashSet<Artigo>();
+        for(Artigo c : vendido){
+            this.vendido.add(c);
+        }
+    }
+
+    public Set<Artigo> getPara_vender() {
+        Set<Artigo> novo = new HashSet<>();
+        for(Artigo c : this.para_vender){
+            novo.add(c);
+        }
+        return novo;
+    }
+
+    public void setPara_vender(Set<Artigo> para_vender) {
+        this.para_vender = new HashSet<Artigo>();
+        for(Artigo c : para_vender){
+            this.para_vender.add(c);
+        }
+    }
+
+    public double getDinheiro_vendas() {
+        return this.dinheiro_vendas;
+    }
+
+    public void setDinheiro_vendas(double dinheiro_vendas) {
+        this.dinheiro_vendas = dinheiro_vendas;
+    }
+
+    public double getDinheiro_compras() {
+        return this.dinheiro_compras;
+    }
+
+    public void setDinheiro_compras(double dinheiro_compras) {
+        this.dinheiro_compras = dinheiro_compras;
+    }
+
+    public void addArtigoVendido(Artigo c){
+        this.vendido.add(c.clone());
+    }
+
+    public void addArtigoParaVender(Artigo c){
+        this.para_vender.add(c.clone());
+    }
+
+    public void removeArtigoParaVender(Artigo c){
+        this.para_vender.remove(c.clone());
+    }
+
+    public Utilizador clone(){
+        return new Utilizador(this);
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Código no Sistema:: " + this.code + "\n");
+        sb.append("Email:: " + this.email + "\n");
+        sb.append("Nome:: " + this.nome + "\n");
+        sb.append("Morada:: " + this.morada + "\n");
+        sb.append("NIF:: " + this.nif + "\n");
+        sb.append("Artigos Vendidos:: [");
+        for(Artigo c : this.vendido){
+            sb.append(c + "\n");
+        }
+        sb.append("]\n");
+        sb.append("Artigos à Venda:: [");
+        for(Artigo c : this.para_vender){
+            sb.append(c + "\n");
+        }
+        sb.append("]\n");
+        sb.append("Dinheiro Feito em Vendas:: " + this.dinheiro_vendas + "\n");
+        sb.append("Dinheiro Gasto em Compras:: " + this.dinheiro_compras + "\n");
+
+        return sb.toString();
+    }
+
+    public boolean isDeepClone(Set<Artigo> set1, Set<Artigo> set2) {
+        if (set1.size() != set2.size()) {
+            return false;
+        }
+        Iterator<Artigo> it = set1.iterator();
+        while (it.hasNext()) {
+            Artigo obj1 = it.next();
+            boolean found = false;
+            for (Artigo obj2 : set2) {
+                if (obj2.equals(obj1)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(Object o){
+        if (this==o) return true;
+        if ((o == null) || (this.getClass() != o.getClass())) return false;
+
+        Utilizador l = (Utilizador) o;
+        return  this.code == l.getCode() &&
+                this.email.equals(l.getEmail()) &&
+                this.nome.equals(l.getNome()) &&
+                this.morada.equals(l.getMorada()) &&
+                this.nif == l.getNif() &&
+                Double.compare(this.dinheiro_compras, l.getDinheiro_compras()) == 0 &&
+                Double.compare(this.dinheiro_vendas, l.getDinheiro_vendas()) == 0 &&
+                isDeepClone(this.para_vender, l.getPara_vender()) &&
+                isDeepClone(this.vendido, l.getVendido());
+
+    }
+}
